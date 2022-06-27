9389a460d0d611664858533440b43c359e3fe381
JaceInnis <79427747+JaceInnis@users.noreply.github.com>
2022-06-26 17:21:32 -0700

    Update ROI.R

--- R/ROI.R
+++ R/ROI.R
@@ -11,12 +11,9 @@ ROI = function( stri = "[117.83526716151925, 15.49233818000526],
           [129.74444684901925, 15.49233818000526]"){
 
   for (i in 1:40) {
-
-
     stringr::str_replace(stri, "\\[","c(") %>%
       stringr::str_replace("\\]","\\)") %>%
       stringr::str_replace("\n"," ") ->stri
-
   }
 
   stri =  paste0("list(",stri,")")
