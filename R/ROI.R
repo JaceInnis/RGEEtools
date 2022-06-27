01c1b7da5365eb9d17294396d197e431a44bbeee
JaceInnis <79427747+JaceInnis@users.noreply.github.com>
2022-06-26 17:38:40 -0700

    forrealthisttime

--- R/ROI.R
+++ R/ROI.R
@@ -11,9 +11,9 @@ ROI = function( stri = "[117.83526716151925, 15.49233818000526],
           [129.74444684901925, 15.49233818000526]"){
 
   for (i in 1:40) {
-    stringr::str_replace(stri, "\\[","c(") %>%
-      stringr::str_replace("\\]","\\)") %>%
-      stringr::str_replace("\n"," ") ->stri
+  stri =   stringr::str_replace(stri, "\\[","c(")
+  stri =     stringr::str_replace("\\]","\\)")
+  stri =     stringr::str_replace("\n"," ")
   }
 
   stri =  paste0("list(",stri,")")
