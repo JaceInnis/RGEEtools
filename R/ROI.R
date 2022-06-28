2adea6af0a9d1f01dbf00e471d185069d07a3399
JaceInnis <79427747+JaceInnis@users.noreply.github.com>
2022-06-28 09:21:15 -0700

    roi should work now

--- R/ROI.R
+++ R/ROI.R
@@ -11,18 +11,18 @@ ROI = function( stri = "[117.83526716151925, 15.49233818000526],
           [129.74444684901925, 15.49233818000526]"){
 
   for (i in 1:40) {
-  stri =   stringr::str_replace(stri, "\\[","c(")
-  stri =     stringr::str_replace("\\]","\\)")
-  stri =     stringr::str_replace("\n"," ")
+    stri =   stringr::str_replace(stri, "\\[","c(")
+    stri =     stringr::str_replace(stri,"\\]","\\)")
+    stri =     stringr::str_replace(stri, "\n"," ")
   }
 
   stri =  paste0("list(",stri,")")
 
   stri = eval(parse(text = stri))
 
-  ROI = ee$Geometry$Polygon(stri)
+  ROI = rgee::ee$Geometry$Polygon(stri)
 
-  Map$addLayer(ROI)
+  rgee::Map$addLayer(ROI)
 
   return(ROI)
 }
